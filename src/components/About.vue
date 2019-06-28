d88383165709deeb936cbfac784f28336b539e5f
miskelley <kalt@unomaha.edu>
2019-06-27 21:55:30 -0500

    Finished assign1

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>Hello, this is {{their_name}}'s skills list developed in Vue.js to learn Vue.js</h1>
+    <p>Vue.js is fun to code in.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+data() {
+  return {
+      their_name: this.$route.params.name
+    }
+  }
+}
+
+</script>
